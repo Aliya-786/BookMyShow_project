208a48f15738af1445ae93cbd14b9cab811fc1d5
Aliya-786 <aliyasultana781@gmail.com>
2026-01-25 21:16:41 +0530

    errorclear

--- src/App.js
+++ src/App.js
@@ -1,62 +1,24 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { movies } from "./data";
-import Movie from "./components/Movie";
-import SeatBooking from "./components/SeatBooking";
-import "./App.css";
-import SelectCity from "./Screens/SelectCity";
-import Splash from "./Screens/Splash";
+import { Routes, Route } from "react-router-dom";
+import axios from "axios";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+import HomePage from "./pages/HomePage";
+import MoviePage from "./pages/MoviePage";
+import PlayPage from "./pages/PlayPage";
+import ErrorPage from "./pages/404";
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.params = {};
+axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
 
 function App() {
-  const [selectedMovie, setSelectedMovie] = useState(null);
-  const [movieList, setMovieList] = useState(movies);
-
-  const handleSelectMovie = (movie) => setSelectedMovie(movie);
-
-  const handleBook = (movieId, seats) => {
-    const updatedMovies = movieList.map((m) =>
-      m.id === movieId
-        ? {
-            ...m,
-            seats: m.seats.map((seat, i) => (seats.includes(i) ? true : seat)),
-          }
-        : m
-    );
-    setMovieList(updatedMovies);
-    alert(`Booked seats: ${seats.map((s) => s + 1).join(", ")}`);
-    setSelectedMovie(null);
-  };
-
   return (
-    <Router>
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<Splash />} />
-          <Route path="/select-city" element={<SelectCity />} />
-          <Route
-            path="/movies"
-            element={
-              <>
-                <h1>BookMyShow Clone</h1>
-                {!selectedMovie ? (
-                  <div className="movie-list">
-                    {movieList.map((movie) => (
-                      <Movie
-                        key={movie.id}
-                        movie={movie}
-                        onSelect={handleSelectMovie}
-                      />
-                    ))}
-                  </div>
-                ) : (
-                  <SeatBooking movie={selectedMovie} onBook={handleBook} />
-                )}
-              </>
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/movie/:id" element={<MoviePage />} />
+      <Route path="/plays" element={<PlayPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
   );
 }
 
--- src/Utils/Colors.js
+++
@@ -1,6 +0,0 @@
-export const useColors={
-    primary:'#D70B17',
-    secondary:'black',
-    white:'white',
-    grey:'grey',
-}
